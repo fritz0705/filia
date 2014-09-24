b78bf567223555cf2193244797912468cf110552
Fritz Grimpen <fritz@grimpen.net>
2014-09-24 18:23:08 +0000

    Implement elasticsearch poc

---
+++ elasticrawl/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"log"
+	"flag"
+
+	"github.com/fritz0705/filia"
+	"github.com/belogik/goes"
+)
+
+func main() {
+	var (
+		flJobs = flag.Int("jobs", 1, "Number of crawler jobs")
+		flHost = flag.String("es-host", "localhost", "Elasticsearch host")
+		flPort = flag.String("es-port", "9200", "Elasticsearch port")
+		flIndex = flag.String("es-index", "filia", "Elasticsearch index")
+		err error
+		crawler = filia.DefaultCrawler
+	)
+
+	flag.Parse()
+
+	esConn := goes.NewConnection(*flHost, *flPort)
+	_, err = esConn.CreateIndex(*flIndex, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	go crawler.Emit(flag.Args()...)
+
+	for n := 0; n < *flJobs; n++ {
+		go crawler.Crawl()
+	}
+
+	for doc := range crawler.Output {
+		esDoc := goes.Document{
+			Index: *flIndex,
+			Type: "document",
+			Fields: doc,
+		}
+
+		_, err = esConn.Index(esDoc, nil)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+}
