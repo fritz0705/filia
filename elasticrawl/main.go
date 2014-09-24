1f5615ecf42f647f453f449893b4079a2661fb47
Fritz Grimpen <fritz@grimpen.net>
2014-09-24 20:16:32 +0000

    Specify timeout for elasticrawl

--- elasticrawl/main.go
+++ elasticrawl/main.go
@@ -3,9 +3,12 @@ package main
 import (
 	"log"
 	"flag"
+	"time"
+	"net/http"
 
 	"github.com/fritz0705/filia"
 	"github.com/belogik/goes"
+	"gopkg.in/fatih/set.v0"
 )
 
 func main() {
@@ -14,13 +17,31 @@ func main() {
 		flHost = flag.String("es-host", "localhost", "Elasticsearch host")
 		flPort = flag.String("es-port", "9200", "Elasticsearch port")
 		flIndex = flag.String("es-index", "filia", "Elasticsearch index")
+		flTimeout = flag.Duration("timeout", 8 * time.Second, "Crawler timeout")
+		flBuffer = flag.Int("buffer", 1<<16, "Internal channel buffer size")
 		err error
-		crawler = filia.DefaultCrawler
 	)
 
-	crawler.Queue = make(filia.StdCrawlerQueue, 1 << 16)
-
 	flag.Parse()
+
+	httpProto := filia.HTTPProto{
+		Client: http.Client{
+			Timeout: *flTimeout,
+		},
+	}
+
+	crawler := filia.Crawler{
+		Settings: filia.Settings{
+			Decoders: filia.DefaultSettings.Decoders,
+			Protos: map[string]filia.Proto{
+				"http": httpProto,
+				"https": httpProto,
+			},
+		},
+		Queue: make(filia.StdCrawlerQueue, *flBuffer),
+		Set: *set.New(),
+		Output: make(chan filia.Document, *flBuffer),
+	}
 
 	esConn := goes.NewConnection(*flHost, *flPort)
 	esConn.CreateIndex(*flIndex, nil)
